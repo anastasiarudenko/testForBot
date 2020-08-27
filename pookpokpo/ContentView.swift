453cc4c3adbab2221f5eb16af8fe6e73b88c70cf
Анастасия Руденко <>
2020-08-27 13:31:40 +0300

    first commit

---
+++ pookpokpo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  pookpokpo
+//
+//  Created by Анастасия Руденко on 27.08.2020.
+//  Copyright © 2020 Анастасия Руденко. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
